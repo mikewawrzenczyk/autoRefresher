3080452c5826e8c75d3f1c9302503d7da665033d
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-08-14 10:06:18 -0400

    updating script.js with refresh and set timeout

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.getElementById('refreshStart').addEventListener('click', refresh)
 
 function refresh(){
-
+    window.setTimeout( function() {
+        window.location.reload();
+      }, 30000);
+    console.log('Page has been reloaded')
 }
