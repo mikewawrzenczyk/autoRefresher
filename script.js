0ccb766313c9b2eb3754fe030f4cabfb244b4264
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-08-15 10:30:14 -0400

    updating script.js with refresh and set timeout

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+//flag to keep refreshing once page has done it, maybe store it in local storage
+// localStorage.setItem('started', 'no');
+
 document.getElementById('refreshStart').addEventListener('click', refresh)
+document.getElementById('refreshStop').addEventListener('click', stopRefresh)
 
 function refresh(){
-    window.setTimeout( function() {
-        window.location.reload();
-      }, 30000);
-    console.log('Page has been reloaded')
+    localStorage.setItem('started', 'yes');
+    while(localStorage.getItem('started' === 'yes')){
+        window.setTimeout( function() {
+            // window.location.reload();
+            console.log('Page has been reloaded')
+          }, 10000);
+    }
+    // console.log('Page has been reloaded')
+}
+
+function stopRefresh(){
+    localStorage.setItem('started', 'no');
 }
